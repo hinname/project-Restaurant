047daaf8a74fb01eab74dd134baeef741849ffd0
All Resende <allanresendegomes@outlook.com>
2022-01-06 16:16:15 -0300

    preparing soil for front-end part 1

---
+++ web-vuejs/src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bigContainer">
+    <div class="header">
+      asasdasd
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
