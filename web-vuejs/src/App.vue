32d7c5325ab9271063ece852776a26e1aae5545e
hinname <herivelton@pop-os.localdomain>
2022-01-11 15:12:30 -0300

    views e router criados

--- web-vuejs/src/App.vue
+++ web-vuejs/src/App.vue
@@ -8,7 +8,7 @@
 
 <script>
 import NavBar from "./components/NavBar.vue"
-import Home from './components/Home.vue'
+import Home from './views/Home.vue'
 
 export default {
   name: 'App',
@@ -33,81 +33,4 @@ export default {
   margin: 0;
   padding: 0;
 }
-
-.bigContainer{
-  overflow-x: hidden;
-  position: absolute;
-  width: 100vw;
-  height: 100%;
-  z-index:1;
-}
-
-.mainContainer{
-  position: relative;
-  width: 85%;
-  height: 100%;
-  left: 15%;
-  display: flex;
-  flex-direction: column;
-  gap: 1vw;
-  background: rgb(238, 234, 226);
-}
-
-.mainContainer *{
-  position: relative;
-}
-
-.header{
-  top:0;
-  height: 8vh;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem;
-  background: whitesmoke;
-}
-
-.header .user{
-  display: flex;
-  gap: 3rem;
-}
-
-.filters{
-  height: 12vh;
-  display: flex;
-  flex-direction: column;
-  gap:1rem;
-  padding: 1rem;
-  background: whitesmoke;
-}
-
-.filterTitle{
-  left: 2rem;
-}
-
-.filterOptions{
-  display: flex;
-  flex-direction: row;
-  gap: 2rem;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-
-.menu{
-  position: relative;
-  display:grid;
-  grid-template-columns: repeat(3, 2fr);
-  justify-items: center;
-  overflow: scroll;
-}
-
-.menu > div{
-  background: white;
-  padding: 1.5rem;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-}
-
 </style>
--- web-vuejs/src/views/Home.vue
+++ web-vuejs/src/views/Home.vue
@@ -45,7 +45,7 @@
 
 <script>
 
-import Dish from "./Dish.vue"
+import Dish from "../components/Dish.vue"
 
 
 export default {
@@ -57,3 +57,81 @@ export default {
 }
 </script>
 
+<style scoped>
+.bigContainer{
+  overflow-x: hidden;
+  position: absolute;
+  width: 100vw;
+  height: 100%;
+  z-index:1;
+}
+
+.mainContainer{
+  position: relative;
+  width: 85%;
+  height: 100%;
+  left: 15%;
+  display: flex;
+  flex-direction: column;
+  gap: 1vw;
+  background: rgb(238, 234, 226);
+}
+
+.mainContainer *{
+  position: relative;
+}
+
+.header{
+  top:0;
+  height: 8vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background: whitesmoke;
+}
+
+.header .user{
+  display: flex;
+  gap: 3rem;
+}
+
+.filters{
+  height: 12vh;
+  display: flex;
+  flex-direction: column;
+  gap:1rem;
+  padding: 1rem;
+  background: whitesmoke;
+}
+
+.filterTitle{
+  left: 2rem;
+}
+
+.filterOptions{
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.menu{
+  position: relative;
+  display:grid;
+  grid-template-columns: repeat(3, 2fr);
+  justify-items: center;
+  overflow: scroll;
+}
+
+.menu > div{
+  background: white;
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+</style>
