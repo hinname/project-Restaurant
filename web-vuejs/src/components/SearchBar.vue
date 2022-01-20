f779b29024348eaa6d37005d857a5fbff7cd3419
Herivelton Costa <herivelton.PRo@gmail.com>
2022-01-20 13:43:34 -0300

    seachbar

---
+++ web-vuejs/src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="barContainer">
+    <fa :icon="['fas', 'search']" /> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar'
+}
+</script>
+
+<style>
+</style>
