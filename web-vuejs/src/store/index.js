83961954e798525c37fd92ce325b7ff141d5e7ec
Herivelton Costa <herivelton.PRo@gmail.com>
2022-01-22 17:04:45 -0300

    store states

--- web-vuejs/src/store/index.js
+++ web-vuejs/src/store/index.js
@@ -2,8 +2,21 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    dark: false,
+    cart: [
+      // Dados de um prato/bebida no carrinho (array de objetos {})
+      // id produto
+      // name
+      // image
+      // ingredients
+      // price
+      // quantity
+    ],
   },
   mutations: {
+    changeDarkMode(state) {
+      state.dark = !state.dark;
+    }
   },
   actions: {
   },
