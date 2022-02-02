9ad338e45eb01e8f0fff80ca1c1ad47d57a7b26a
hinname <herivelton.pro@gmail.com>
2022-02-02 10:17:51 -0300

    search change

--- node/src/services/Dish/GetFilteredDishesService.js
+++ node/src/services/Dish/GetFilteredDishesService.js
@@ -1,13 +1,15 @@
 
 class GetFilteredDishesService {
   async execute(query) {
-    const dishes = require('../../../info.json')
+    const dishes = require('../../../info.json');
+
+    query = query.toLowerCase();
 
     const filteredDishes = dishes.filter((dish) => {
-      return dish.nome.includes(query) //|| dish.filter.includes(query)
+      return dish.nome.toLowerCase().includes(query) //|| dish.filter.includes(query)
     })
 
-    return filteredDishes
+    return filteredDishes;
   }
 }
 
